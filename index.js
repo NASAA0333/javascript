36faa4eae0b85dbbec765626836992f0847033cf
NASAA0333 <nasanbuyan0333@gmail.com>
2024-11-04 19:20:06 +0800

    10-4

---
+++ index.js
@@ -0,0 +1,50 @@
+// let a = 1;
+// let r = 9;
+// while (a < 9) {
+//   z = a * r;
+//   a = a + 1;
+//   console.log(a, "*", r, "=", z + r);
+// }
+// console.log("bodlogo22");
+// let number = 3;
+// let maximumnumber = 37;
+// while (number <= maximumnumber) {
+//   number++;
+//   if (number % 5 == 0) {
+//     console.log(number);
+//   }
+// }
+
+// 23;
+// console.log("bodlogo23");
+
+// let z = 5234;
+// let b,
+//   mult = 1;
+// //c=536,e=53
+// //b=4,d=6,e=3,h=5
+// while (z > 0) {
+//   b = z % 10;
+//   if (b % 2 == 0) {
+//     mult *= b;
+//   }
+//   z = (z - (z % 10)) / 10;
+// }
+// console.log(mult);
+
+// 26;
+// console.log("bodlogo26");
+
+// let N = 1003;
+// let count = 0;
+// while (N != 1) {
+//   if (N % 2 == 0) {
+//     N = N / 2;
+//   } else if (N % 2 == 1) {
+//     N = N * 3 + 1;
+//   }
+//   count++;
+// }
+// console.log(count);
+
+// 27;
