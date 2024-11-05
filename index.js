a24a4fc39230969df1a5c183deaf48657430c2dc
NASAA0333 <nasanbuyan0333@gmail.com>
2024-11-05 18:51:05 +0800

    10-5

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-console.log("asd");
+// ---*
+// --*-*
+// -*-*-*
+// *-*-*-*
+// const n = 5;
+// let result = "";
+// for (a = 0; a < n; a++) {
+//   result = result + "* ";
+//   console.log(result);
+// }
+
+// let result = " ";
+// for (let i = 0; i < n; i++) {
+//   for (let q = 0; q < n - i - 1; q++) {
+//     result = result + " ";
+//   }
+//   for (let w = 0; w < i; w++) {
+//     result = result + "* ";
+//   }
+//   console.log(result);
+//   result = " ";
+// }
+const n = 5;
+for (let i = 0; i <= n; i++) {
+  let row = " ".repeat(n - i) + "* ".repeat(i);
+  console.log(row);
+}
