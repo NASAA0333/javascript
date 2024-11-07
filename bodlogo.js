2c6f883816316627de3cfacc1f5745150261fb6e
NASAA0333 <nasanbuyan0333@gmail.com>
2024-11-07 17:56:57 +0800

    10-6

--- bodlogo.js
+++ bodlogo.js
@@ -227,3 +227,112 @@
 // }
 // prinTicketInfo("Playtime", "300000MNT", "20%");
 // console.log("-----------Midium-level-----------");
+
+// function filterBooksByYear(year) {
+//   const books = [
+//     { title: "To Kill a Mockingbird", author: "Harper Lee", year: 1960 },
+//     { title: "1984", author: "George Orwell", year: 1949 },
+//     { title: "The Great Gatsby", author: "F. Scott Fitzgerald", year: 1925 },
+//     {
+//       title: "One Hundred Years of Solitude",
+//       author: "Gabriel Garcia Marquez",
+//       year: 1967,
+//     },
+//     { title: "Pride and Prejudice", author: "Jane Austen", year: 1813 },
+//   ];
+//   console.log(title, author);
+//   return [year];
+// }
+
+// Exercise 1
+// Write a function filterBooksByYear that takes a year as arguments,
+//  and returns a new array containing only the books that were
+//  published on or after the given year.
+// Example data:
+// const books = [   { title: "To Kill a Mockingbird", author:
+// "Harper Lee", year: 1960 }, { title: "1984", author: "George Orwell", year: 1949 },
+// { title: "The Great Gatsby", author: "F. Scott Fitzgerald", year: 1925 }, {
+//   title:"One Hundred Years of Solitude", author: "Gabriel Garcia Marquez", year: 1967
+// }, { title: "Pride and Prejudice", author: "Jane Austen", year: 1813 }];
+// When Filter by year 1950
+// Output:
+// [   { title: "To Kill a Mockingbird", author: "Harper Lee", year: 1960 },
+// { title: "One Hundred Years of Solitude", author: "Gabriel Garcia Marquez", year: 1967 }]
+// const book1 = {
+//   title: "To Kill a Mockingbird",
+//   author: "Harper Lee",
+//   year: 1960,
+// };
+// const book2 = { title: "1984", author: "George Orwell", year: 1949 };
+// const book3 = {
+//   title: "The Great Gatsby",
+//   author: "F. Scott Fitzgerald",
+//   year: 1925,
+// };
+// const book4 = {
+//   title: "Pride and Prejudice",
+//   author: "Jane Austen",
+//   year: 1813,
+// };
+// const books = { book1, book2, book3, book4 };
+// let = 1;
+// i = 0;
+// books[0];
+
+// i = 1;
+// books[1];
+
+// i = 2;
+// books[2];
+
+// i = 3;
+// books[3];
+
+// for (i = 0; i < books.length; i++) {}
+// function filterBooksByYear(filterYear) {
+//   let filterBooksByYear = [];
+//   for (let i = 0; i < books.length; i++) {}
+//   const
+// }
+
+// let totalTitles = "";
+// console.log(books.title);
+
+// todo.js
+
+// let todos = [
+//   {
+//     name: "Geree tseverleh",
+//     status: "TODO",
+//   },
+// ];
+
+// //Todo add
+// function addOne(newTodo) {
+//   todos.push(newTodo);
+// }
+
+// // status uurcluh :func
+// function editStatus(index, status) {
+//   let item = todos[index];
+//   item.status = status;
+// }
+
+// // ner uurcluh : func
+// function editName(index, name) {
+//   let item = todos[index];
+//   item.name = name;
+// }
+
+// // doto delete one item
+// function deleteOne() {}
+
+// // RUNNING APPLICATION
+// addOne({ name: "Hool hiih ", status: "TODO" });
+// addOne({ name: "JS sudlah ", status: "TODO" });
+// addOne({ name: "13579 ", status: "TODO" });
+// addOne({ name: "2468 ", status: "TOd" });
+// editStatus(1, "Dones");
+// console.log(todos);
+
+// // filter books
--- index.js
+++ index.js
@@ -1,64 +1,45 @@
-// function filterBooksByYear(year) {
-//   const books = [
-//     { title: "To Kill a Mockingbird", author: "Harper Lee", year: 1960 },
-//     { title: "1984", author: "George Orwell", year: 1949 },
-//     { title: "The Great Gatsby", author: "F. Scott Fitzgerald", year: 1925 },
-//     {
-//       title: "One Hundred Years of Solitude",
-//       author: "Gabriel Garcia Marquez",
-//       year: 1967,
-//     },
-//     { title: "Pride and Prejudice", author: "Jane Austen", year: 1813 },
-//   ];
-//   console.log(title, author);
-//   return [year];
-// }
+// todo.js
 
-// Exercise 1
-// Write a function filterBooksByYear that takes a year as arguments,
-//  and returns a new array containing only the books that were
-//  published on or after the given year.
-// Example data:
-// const books = [   { title: "To Kill a Mockingbird", author:
-// "Harper Lee", year: 1960 }, { title: "1984", author: "George Orwell", year: 1949 },
-// { title: "The Great Gatsby", author: "F. Scott Fitzgerald", year: 1925 }, {
-//   title:"One Hundred Years of Solitude", author: "Gabriel Garcia Marquez", year: 1967
-// }, { title: "Pride and Prejudice", author: "Jane Austen", year: 1813 }];
-// When Filter by year 1950
-// Output:
-// [   { title: "To Kill a Mockingbird", author: "Harper Lee", year: 1960 },
-// { title: "One Hundred Years of Solitude", author: "Gabriel Garcia Marquez", year: 1967 }]
-const book1 = {
-  title: "To Kill a Mockingbird",
-  author: "Harper Lee",
-  year: 1960,
-};
-const book2 = { title: "1984", author: "George Orwell", year: 1949 };
-const book3 = {
-  title: "The Great Gatsby",
-  author: "F. Scott Fitzgerald",
-  year: 1925,
-};
-const book4 = {
-  title: "Pride and Prejudice",
-  author: "Jane Austen",
-  year: 1813,
-};
-const books = { book1, book2, book3, book4 };
-let = 1;
-i = 0;
-books[0];
+let todos = [];
 
-i = 1;
-books[1];
+//Todo add
+function addOne(newTodo) {
+  todos.push(newTodo);
+}
 
-i = 2;
-books[2];
+// status uurcluh :func
+function editStatus(index, status) {
+  let item = todos[index];
+  item.status = status;
+}
 
-i = 3;
-books[3];
+// ner uurcluh : func
+function editName(index, name) {
+  let item = todos[index];
+  item.name = name;
+}
 
-for (i = 0; i < books.length; i++) {}
+// doto delete one item
+function deleteOne(index) {
+  let arr = [];
+  for (i = 0; i < todos.length; i++) {
+    if (i != index) {
+      arr.push(todos[i]);
+    }
+  }
+  todos = arr;
+}
+// todo delete all
+function deleteAll() {
+  todos = [];
+}
 
-let totalTitles = "";
-console.log(books.title);
+// RUNNING APPLICATION
+addOne({ name: "Geree tseverleh", status: "TODO" });
+addOne({ name: "Hool hiih ", status: "TODO" });
+addOne({ name: "JS sudlah ", status: "TODO" });
+addOne({ name: "13579 ", status: "TODO" });
+editStatus(1, "Dones");
+console.log(todos);
+
+// filter books
